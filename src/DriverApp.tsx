6bee657abb460256f93d5bd03a284feb1700903c
Byeonggi Kwak <asbck@naver.com>
2024-09-25 16:53:28 +0900

    Driver 완료

---
+++ src/DriverApp.tsx
@@ -0,0 +1,81 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Alert} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useEffect} from 'react';
+import messaging from '@react-native-firebase/messaging';
+
+import Intro from './Intro.tsx';
+import Main from './Main.tsx';
+import Login from './Login.tsx';
+import Register from './Register.tsx';
+import NickNameScreen from './Main_Setting_NickName.tsx';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('[background remote message]', remoteMessage);
+});
+
+function DriverApp(): JSX.Element {
+  console.log('-- DriverApp()');
+  const Stack = createStackNavigator();
+
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    await AsyncStorage.setItem('fcmToken', fcmToken);
+    console.log('>> fcmToken = ' + fcmToken);
+  };
+
+  useEffect(() => {
+    getFcmToken();
+
+    messaging().onMessage(remoteMessage => {
+      console.log('[remote message]', JSON.stringify(remoteMessage));
+      let title = '';
+      let body = '';
+
+      if (remoteMessage.notification && remoteMessage.notification.title) {
+        title = remoteMessage.notification.title;
+      }
+      if (remoteMessage.notification && remoteMessage.notification.body) {
+        body = remoteMessage.notification.body;
+      }
+      if (remoteMessage) {
+        Alert.alert(title, body, [{text: '확인', style: 'cancel'}]);
+      }
+    });
+  });
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Intro"
+          component={Intro}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{headerShown: true, title: '회원가입'}}
+        />
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="NickName"
+          component={NickNameScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default DriverApp;
